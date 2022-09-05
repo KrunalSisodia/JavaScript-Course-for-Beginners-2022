e3017e3a23230c24822e92e0a49268621100f4cd
KrunalSisodia <krunalsisodia@gmail.com>
2022-09-05 13:16:23 +0530

    Tip Calculator

---
+++ tipCalculator.js
@@ -0,0 +1,10 @@
+// food = "banana";
+// console.log(food);
+
+// Tip calculator
+
+foodAmount = Number(prompt("How much was the food?"));
+tipPercentage = Number(prompt("tip %") / 100);
+totalTip = foodAmount * tipPercentage;
+totalAmount = foodAmount + totalTip;
+console.log(totalAmount);
